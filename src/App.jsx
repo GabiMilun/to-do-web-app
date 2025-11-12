38cd70a25113b33e0f4b3f5d1b1b5842f2c05386
DESPIKABLEEE <grafikaking.gabriel@gmail.com>
2025-11-12 14:45:01 +0100

    implementiran mobx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,39 @@
-import './App.css'
+import React, { useState } from 'react'
+import { observer } from 'mobx-react-lite'
+import todoStore from './stores/todoStore'
 
 function App() {
+  const [text, setText] = useState('')
+
+  function submit(e) {
+    e.preventDefault()
+    todoStore.addTodo(text)
+    setText('')
+  }
 
   return (
+
     <>
-      pozdrav
+      <h1>To-do lista</h1>
+      <form onSubmit={submit}>
+        <input type="text" name='naziv-to-do' placeholder='Unesite novi to-do' onChange={(e) => setText(e.target.value)} />
+        <button type='submit'>Dodaj to-do</button>
+      </form>
+      <ul>
+        {todoStore.todos.length == 0 && <li>Lista je prazna</li>}
+        {todoStore.todos.map(todo => (
+          <li key={todo.id}>
+            <input type="checkbox" checked={todo.done} onChange={() => todoStore.toggleTodo(todo.id)} />
+            <span>{todo.title}</span>
+            <button onClick={() => todoStore.removeTodo(todo.id)}>Izbrisi me</button>
+          </li>
+          
+        ))}
+      </ul>
+      {todoStore.todos.length > 0 && <h3>Lista ima {todoStore.todos.length} stavki</h3>}
     </>
+    
   )
 }
 
-export default App
+export default observer(App)
--- src/stores/todoStore.js
+++ src/stores/todoStore.js
@@ -1,11 +1,57 @@
 import { makeAutoObservable } from 'mobx'
 
 class TodoStore {
-  todos = []
+//   todos = []
 
-  constructor() {
-    makeAutoObservable(this)
-  }
+//   constructor() {
+//     makeAutoObservable(this)
+//   }
+
+//   addTodo(title) {
+//     const trimmed = String(title || '').trim()
+//     if (!trimmed) return
+
+//     this.todos.push({
+//       id: Date.now(),
+//       title: trimmed,
+//       done: false,
+//     })
+//   }
+
+//   removeTodo(id) {
+//     this.todos = this.todos.filter(t => t.id !== id)
+//   }
+
+//   toggleTodo(id) {
+//     const t = this.todos.find(x => x.id === id)
+//     if (t) t.done = !t.done
+//   }
+
+
+    todos = []
+
+    constructor(){
+        makeAutoObservable(this);
+    }
+
+    addTodo(title){
+        this.todos.push({
+            id: Date.now(),
+            title: title,
+            done: false
+        })
+    }
+
+    removeTodo(id){
+        this.todos = this.todos.filter(x => x.id != id);
+    }
+
+    toggleTodo(id){
+        const x = this.todos.find(e => e.id == id);
+        if (x){
+            x.done = !x.done;
+        }
+    }
 
 }
 
